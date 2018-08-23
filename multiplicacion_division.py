f2e94370a1ae2702a339e2c8d20f45a54c6d6f84
Javier Barreda <javib94usac@github.com>
2018-08-23 01:11:50 -0600

    Hotfix Division

--- multiplicacion_division.py
+++ multiplicacion_division.py
@@ -1,8 +1,10 @@
-# Funciones de multiplicaciónn y división
+﻿# Funciones de multiplicaciónn y división
 # Marvin José Calderón García 201213177
 
 def multiplicacion(num1, num2):
 	return num1*num2
 	
 def division(num1, num2):
-	return num1/num2
+	if (num2>0) 
+		return num1/num2
+	return 0
