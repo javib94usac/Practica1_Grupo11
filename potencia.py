fb267374869537b195a1856d6df8ca3e4865b8f1
ricardcutzh <ricardcutzh@gmail.com>
2018-08-22 23:53:12 -0600

    Funcionalidad de Raiz Cuadrada

--- potencia.py
+++ potencia.py
@@ -1,3 +1,9 @@
+import math
+
 def potencia_cuadra(num):
 	return num*num
 
+#FUNCION ENCARGADA DE CALCULAR LA RAIZ CUADRADA DE UN NUMERO
+# Ricardo Antonio Cutz Hernandez 201503476
+def raiz_cuadrada(numero):
+    print(math.sqrt(numero))
